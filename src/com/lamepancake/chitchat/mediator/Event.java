29c2e8c9ae70069bacfe5a7a734b4762e40614de
Shane Spoor <shane.d.spoor@gmail.com>
2014-10-13 12:56:48 -0700

    Add WHOISIN and WAITINLIST to Event

--- src/com/lamepancake/chitchat/mediator/Event.java
+++ src/com/lamepancake/chitchat/mediator/Event.java
@@ -16,6 +16,9 @@ public class Event {
     public static final int JOIN = 3;
     public static final int LEAVE = 4;
     public static final int GRANT = 5;
+    public static final int CHATLIST = 6;
+    public static final int WHOISIN = 7;
+    public static final int WAITINGLIST = 8;
     
     private final int type;
     private final Object arg;
