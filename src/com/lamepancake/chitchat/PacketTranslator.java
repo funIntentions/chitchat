100bf15c80047cdba7bcae1dd52d34ff36f6de01
dan <danr94@gmail.com>
2014-10-13 09:42:28 -0700

    packet translator added

---
+++ src/com/lamepancake/chitchat/PacketTranslator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lamepancake.chitchat;
+import com.lamepancake.chitchat.packet.Packet;
+import java.util.Observable;
+
+/**
+ *
+ * @author dan
+ */
+public class PacketTranslator extends Observable 
+{
+    void translate(Packet packet)
+    {
+        
+    }
+    
+    //Tests if this object has changed.
+    //void notifyObservers()
+    //{
+    //    
+    //}
+    
+    //If this object has changed, as indicated by the hasChanged method, then notify all of its observers and then call the clearChanged method to indicate that this object has no longer changed.
+    //void notifyObservers(Object arg)
+    //{
+    //    
+    //}
+}
