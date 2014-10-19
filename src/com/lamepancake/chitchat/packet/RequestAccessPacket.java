4ec952e5d077b12e4ce5b2bae6fc9f9b17b34028
twarrre <tw4194@hotmail.com>
2014-10-18 17:24:10 -0700

    finished some packets

--- src/com/lamepancake/chitchat/packet/RequestAccessPacket.java
+++ src/com/lamepancake/chitchat/packet/RequestAccessPacket.java
@@ -13,12 +13,50 @@ import java.nio.ByteBuffer;
  */
 public class RequestAccessPacket extends Packet
 {
-    public RequestAccessPacket()
+    private final int userID;
+    private final int chatID;
+
+    public RequestAccessPacket(int userid, int chatid)
     {
-        super(REQUESTACCESS, 0);
+        super(REQUESTACCESS, 8);
+        this.userID = userid;
+        this.chatID = chatid;
     }
 
-    public RequestAccessPacket(ByteBuffer packetHeader, ByteBuffer packetData) {
-        super(REQUESTACCESS, 0);
+    public RequestAccessPacket(ByteBuffer packetHeader, ByteBuffer packetData)
+    {
+        super(packetHeader);
+        this.userID = packetData.getInt();
+        this.chatID = packetData.getInt();
+    }
+    
+    @Override
+    public ByteBuffer serialise()
+    {
+        ByteBuffer buf = super.serialise();
+        buf.putInt(this.userID);
+        buf.putInt(this.chatID);
+        
+        buf.rewind();
+        
+        return buf;
+    }
+    
+    /**
+     * Gets the user ID of the user who wants access to a chat.
+     * @return The user ID of the user who wants access to a chat.
+     */
+    public int getUserID()
+    {
+        return userID;
+    }
+    
+     /**
+     * Gets the chat ID of the chat they want access to.
+     * @return The chat ID of the chat they want access to.
+     */
+    public int getChatID()
+    {
+        return chatID;
     }
 }
