71bad16204c72fbb1fe1fd035856d91c5d99a3f5
twarrre <tw4194@hotmail.com>
2014-10-19 12:26:41 -0700

    Fixed the whoisin packet again

--- src/com/lamepancake/chitchat/packet/WhoIsInPacket.java
+++ src/com/lamepancake/chitchat/packet/WhoIsInPacket.java
@@ -8,8 +8,9 @@ package com.lamepancake.chitchat.packet;
 import com.lamepancake.chitchat.User;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Sent to request or transmit a list of all users in a given chat.
@@ -34,10 +35,12 @@ public class WhoIsInPacket extends Packet {
     /**
      * The list of users in the requested list.
      */
-    private List<User> users;
+    //private List<User> users;
     
-    private List<User> onlineUsers;
-        
+    //private List<User> onlineUsers;
+    
+    private Map<User, Boolean> users;
+    
     /**
      * ID of the chat that's being requested for information.
      */
@@ -57,7 +60,7 @@ public class WhoIsInPacket extends Packet {
     public WhoIsInPacket(int chatID, int userID)
     {
         // The 4 is already added in the other constructor, so don't repeat it here
-        this(null, 0, null, chatID, userID);
+        this(null, 0, chatID, userID);
     }
     
     /**
@@ -66,15 +69,13 @@ public class WhoIsInPacket extends Packet {
      * 
      * @param users  The list of all users for the specified chat.
      * @param length The length, in bytes, of the data portion of the packet.
-     * @param onlineUsers The list of all users that are online in the specified chat.
      * @param chatID The ID of the chat containing the user list.
      * @param userID The ID of the user requesting or receiving the list.
      */
-    public WhoIsInPacket(List<User> users, int length, List<User> onlineUsers, int chatID, int userID)
+    public WhoIsInPacket(Map<User, Boolean> users, int length, int chatID, int userID)
     {
-        super(WHOISIN, length + (4 * users.size()) + 12);
+        super(WHOISIN, length + 12);
         this.users = users;
-        this.onlineUsers = onlineUsers;
         this.chatID = chatID;
         this.userID = userID;
     }
@@ -97,29 +98,30 @@ public class WhoIsInPacket extends Packet {
         
         if(listLen > 0)
         {
-            this.users = new ArrayList<>(listLen);
-            this.onlineUsers = new ArrayList<>();
+            this.users = new HashMap<>(listLen);
 
             for(int i = 0; i < listLen; i++)
             {
                 int     nameLen  = data.getInt();
                 int     role;
                 int     id;
-                int     onlineStatus;
+                byte    onlineStatus;
                 byte[]  rawName  = new byte[nameLen];
                 String  username;
                 data.get(rawName);
                 username = new String(rawName, StandardCharsets.UTF_16LE);
                 role = data.getInt();
                 id = data.getInt();
-                onlineStatus = data.getInt();
+                onlineStatus = data.get();
                 
                 if(onlineStatus == 1)
                 {
-                    onlineUsers.add(new User().setName(username).setRole(role).setID(id));
+                    users.put(new User().setName(username).setRole(role).setID(id), true); 
                 }
-
-                users.add(new User().setName(username).setRole(role).setID(id));            
+                else
+                {
+                    users.put(new User().setName(username).setRole(role).setID(id), false);  
+                }
             }
         }
     }
@@ -129,21 +131,11 @@ public class WhoIsInPacket extends Packet {
      * 
      * @return The list of users in this WhoIsInPacket. 
      */
-    public List<User> getUsers()
+    public Map<User, Boolean> getUsers()
     {
         return this.users;
     }
-    
-    /**
-     * Get the list of online users contained in this WhoIsInPacket.
-     * 
-     * @return The list of online users in this WhoIsInPacket. 
-     */
-    public List<User> getOnlineUsers()
-    {
-        return this.onlineUsers;
-    }
-    
+       
     
     @Override
     public ByteBuffer serialise()
@@ -160,30 +152,26 @@ public class WhoIsInPacket extends Packet {
         
         // Structure is: {numUsers}[{nameLength}{name}{role}{id}{onlineStatus}]
         buf.putInt(this.users.size());
-        for(User u : this.users)
+        Set<User> keys = users.keySet();
+        
+        for(User u : keys)
         {
-            boolean isOnline = false;
             String username = u.getName();
 
             buf.putInt(username.length() * 2);
             buf.put(username.getBytes(StandardCharsets.UTF_16LE));
             buf.putInt(u.getRole());
             buf.putInt(u.getID());
-            
-            for(User us : this.onlineUsers)
+            Byte b;
+            if(users.get(u))
             {
-                if(u.getID() == us.getID())
-                {
-                    buf.putInt(1);
-                    isOnline = true;
-                    break;
-                }
+                b = 1;
             }
-            
-            if(!isOnline)
+            else
             {
-                buf.putInt(0);
+                b = 0;
             }
+            buf.put(b);
         }
         
         buf.rewind();
